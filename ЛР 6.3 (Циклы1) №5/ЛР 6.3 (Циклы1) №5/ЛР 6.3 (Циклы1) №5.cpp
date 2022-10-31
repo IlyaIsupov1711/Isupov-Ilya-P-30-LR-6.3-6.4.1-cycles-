63d356cb7fc3cbd351284ad059bcccde1274417b
Admin <isupovilya1711@yandex.ru>
2022-10-31 19:15:41 +0300

    1

---
+++ ЛР 6.3 (Циклы1) №5/ЛР 6.3 (Циклы1) №5/ЛР 6.3 (Циклы1) №5.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	int num;
+	std::cout << "Введите число="; std::cin >> num;
+    switch (num)
+    {
+    case 1: std::cout << "Январь";
+        break;
+    case 2: std::cout << "Февраль";
+        break;
+    case 3: std::cout << "Март";
+        break;
+    case 4: std::cout << "Апрель";
+        break;
+    case 5: std::cout << "Май";
+        break;
+    case 6: std::cout << "Июнь";
+        break;
+    case 7: std::cout << "Июль";
+        break;
+    case 8: std::cout << "Август";
+        break;
+    case 9: std::cout << "Сентябрь";
+        break;
+    case 10: std::cout << "Октябрь";
+        break;
+    case 11: std::cout << "Ноябрь";
+        break;
+    case 12: std::cout << "Декабрь";
+        break;
+    };
+}
---
+++ ЛР 6.4.1 (Циклы1) №5/ЛР 6.4.1 (Циклы2) №5/ЛР 6.4.1 (Циклы2) №5.cpp
@@ -0,0 +1,16 @@
+﻿#include<iostream>
+
+int main()
+{
+    setlocale(LC_ALL, "RUS");
+    int gouses, rabits;
+    gouses = 0;
+    do
+    {
+        rabits = (64 - gouses * 2) / 4;
+        if (rabits * 4 + gouses * 2 == 64)
+            std::cout << "\n" << gouses << " гусей и  " << rabits << "кроликов";
+        gouses++;
+    } while (gouses <= 32);
+    return 0;
+}
